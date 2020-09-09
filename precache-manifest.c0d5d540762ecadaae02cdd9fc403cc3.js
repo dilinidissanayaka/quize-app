45a872bf5d32337c2add1d0ba0f17707d27c3283
dilini <dilinidissanayak18@gmail.com>
2020-09-09 19:16:19 +0530

    Updates

---
+++ precache-manifest.c0d5d540762ecadaae02cdd9fc403cc3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f725eb9db9cdb09db99d23e505c21493",
+    "url": "/quize-app/index.html"
+  },
+  {
+    "revision": "5c48354e3c00c387b651",
+    "url": "/quize-app/static/css/main.17904249.chunk.css"
+  },
+  {
+    "revision": "c5e96a6a287cc60499f3",
+    "url": "/quize-app/static/js/2.1847e0ee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quize-app/static/js/2.1847e0ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c48354e3c00c387b651",
+    "url": "/quize-app/static/js/main.3901a1da.chunk.js"
+  },
+  {
+    "revision": "fbd4c02c1694cc63940c",
+    "url": "/quize-app/static/js/runtime-main.7b896a28.js"
+  }
+]);
